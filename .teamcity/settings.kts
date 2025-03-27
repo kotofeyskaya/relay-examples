b2282b562452d6768ac2bafb4e73c751a486ddae
kotofeyskaya <catherine.kh@ya.ru>
2025-03-27 15:29:57 +0200

    Add generated settings

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -3,6 +3,10 @@ import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
+import jetbrains.buildServer.configs.kotlin.buildFeatures.notifications
+import jetbrains.buildServer.configs.kotlin.buildSteps.powerShell
+import jetbrains.buildServer.configs.kotlin.failureConditions.BuildFailureOnMetric
+import jetbrains.buildServer.configs.kotlin.failureConditions.failOnMetricChange
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -29,69 +33,98 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2024.12"
 
 project {
-    description = "Relay Examples Project"
+    description = "Relay Examples - A collection of example applications using Relay"
 
-    // Define VCS Root
-    val vcsRoot = GitVcsRoot {
-        id("RelayExamplesVcs")
-        name = "relay-examples git repository"
-        url = "https://github.com/relayjs/relay-examples.git"
-        branch = "refs/heads/main"
-    }
-    vcsRoot(vcsRoot)
+    // VCS Root definition
+    vcsRoot(RelayExamplesVcs)
 
-    // Define Build Configuration
-    buildType {
-        id("RelayExamplesBuild")
-        name = "Build Relay Examples"
-
-        vcs {
-            root(vcsRoot)
-        }
-
-        // Build Steps
-        steps {
-            // Install dependencies using yarn
-            script {
-                name = "Install Dependencies"
-                scriptContent = "yarn install"
-                workingDir = "todo"
-            }
-
-            // Build the project using yarn
-            script {
-                name = "Build Project"
-                scriptContent = "yarn build"
-                workingDir = "todo"
-            }
-
-            // Run tests if needed
-            script {
-                name = "Run Linting"
-                scriptContent = "yarn lint"
-                workingDir = "todo"
-            }
-        }
-
-        // Triggers
-        triggers {
-            vcs {
-                branchFilter = "+:*"
-            }
-        }
-
-        // Features
-        features {
-            perfmon {
-            }
-        }
-
-        // Artifacts
-        artifactRules = """
-            todo/__generated__/relay => relay-artifacts.zip
-            todo/__generated__/queries.json => queries.json
-            todo/data => data.zip
-            todo/js => js.zip
-        """.trimIndent()
-    }
+    // Build configuration
+    buildType(RelayExamplesBuild)
 }
+
+object RelayExamplesVcs : GitVcsRoot({
+    name = "Relay Examples"
+    url = "https://github.com/relayjs/relay-examples.git"
+    branch = "refs/heads/main"
+})
+
+object RelayExamplesBuild : BuildType({
+    name = "Build Relay Examples"
+    description = "Build all Relay example applications using yarn"
+
+    vcs {
+        root(RelayExamplesVcs)
+    }
+
+    steps {
+        // Build todo example
+        script {
+            name = "Build Todo Example"
+            workingDir = "todo"
+            scriptContent = """
+                yarn install
+                yarn build
+            """.trimIndent()
+        }
+
+        // Build issue-tracker example
+        script {
+            name = "Build Issue Tracker Example"
+            workingDir = "issue-tracker"
+            scriptContent = """
+                yarn install
+                yarn build
+            """.trimIndent()
+        }
+
+        // Build issue-tracker-next-v13 example
+        script {
+            name = "Build Issue Tracker Next v13 Example"
+            workingDir = "issue-tracker-next-v13"
+            scriptContent = """
+                yarn install
+                yarn build
+            """.trimIndent()
+        }
+
+        // Build data-driven-dependencies example
+        script {
+            name = "Build Data Driven Dependencies Example"
+            workingDir = "data-driven-dependencies"
+            scriptContent = """
+                yarn install
+                yarn build
+            """.trimIndent()
+        }
+
+        // Build newsfeed example
+        script {
+            name = "Build Newsfeed Example"
+            workingDir = "newsfeed"
+            scriptContent = """
+                yarn install
+                yarn build
+            """.trimIndent()
+        }
+    }
+
+    triggers {
+        vcs {
+            branchFilter = "+:*"
+        }
+    }
+
+    features {
+        perfmon {
+        }
+    }
+
+    // Publish artifacts
+    artifactRules = """
+        todo => todo.zip
+        issue-tracker => issue-tracker.zip
+        issue-tracker-next-v13 => issue-tracker-next-v13.zip
+        data-driven-dependencies => data-driven-dependencies.zip
+        newsfeed => newsfeed.zip
+    """.trimIndent()
+})
